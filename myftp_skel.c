f5859b29644258afce126701b4bb773c13a7b670
Santiago Rotman <SantiagoNicolasRotman@gmail.com>
2020-04-19 16:11:08 -0300

    Agrego funciones verificar IP y Puerto

---
+++ myftp_skel.c
@@ -0,0 +1,251 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <err.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define BUFSIZE 512
+
+/**
+ * function: receive and analize the answer from the server
+ * sd: socket descriptor
+ * code: three leter numerical code to check if received
+ * text: normally NULL but if a pointer if received as parameter
+ *       then a copy of the optional message from the response
+ *       is copied
+ * return: result of code checking
+ **/
+bool recv_msg(int sd, int code, char *text) {
+    char buffer[BUFSIZE], message[BUFSIZE];
+    int recv_s, recv_code;
+
+    // receive the answer
+
+
+    // error checking
+    if (recv_s < 0) warn("error receiving data");
+    if (recv_s == 0) errx(1, "connection closed by host");
+
+    // parsing the code and message receive from the answer
+    sscanf(buffer, "%d %[^\r\n]\r\n", &recv_code, message);
+    printf("%d %s\n", recv_code, message);
+    // optional copy of parameters
+    if(text) strcpy(text, message);
+    // boolean test for the code
+    return (code == recv_code) ? true : false;
+}
+
+/**
+ * function: send command formated to the server
+ * sd: socket descriptor
+ * operation: four letters command
+ * param: command parameters
+ **/
+void send_msg(int sd, char *operation, char *param) {
+    char buffer[BUFSIZE] = "";
+
+    // command formating
+    if (param != NULL)
+        sprintf(buffer, "%s %s\r\n", operation, param);
+    else
+        sprintf(buffer, "%s\r\n", operation);
+
+    // send command and check for errors
+
+}
+
+/**
+ * function: simple input from keyboard
+ * return: input without ENTER key
+ **/
+char * read_input() {
+    char *input = malloc(BUFSIZE);
+    if (fgets(input, BUFSIZE, stdin)) {
+        return strtok(input, "\n");
+    }
+    return NULL;
+}
+
+/**
+ * function: login process from the client side
+ * sd: socket descriptor
+ **/
+void authenticate(int sd) {
+    char *input, desc[100];
+    int code;
+
+    // ask for user
+    printf("username: ");
+    input = read_input();
+
+    // send the command to the server
+
+    // relese memory
+    free(input);
+
+    // wait to receive password requirement and check for errors
+
+
+    // ask for password
+    printf("passwd: ");
+    input = read_input();
+
+    // send the command to the server
+
+
+    // release memory
+    free(input);
+
+    // wait for answer and process it and check for errors
+
+}
+
+/**
+ * function: operation get
+ * sd: socket descriptor
+ * file_name: file name to get from the server
+ **/
+void get(int sd, char *file_name) {
+    char desc[BUFSIZE], buffer[BUFSIZE];
+    int f_size, recv_s, r_size = BUFSIZE;
+    FILE *file;
+
+    // send the RETR command to the server
+
+    // check for the response
+
+    // parsing the file size from the answer received
+    // "File %s size %ld bytes"
+    sscanf(buffer, "File %*s size %d bytes", &f_size);
+
+    // open the file to write
+    file = fopen(file_name, "w");
+
+    //receive the file
+
+
+
+    // close the file
+    fclose(file);
+
+    // receive the OK from the server
+
+}
+
+/**
+ * function: operation quit
+ * sd: socket descriptor
+ **/
+void quit(int sd) {
+    // send command QUIT to the client
+
+    // receive the answer from the server
+
+}
+
+/**
+ * function: make all operations (get|quit)
+ * sd: socket descriptor
+ **/
+void operate(int sd) {
+    char *input, *op, *param;
+
+    while (true) {
+        printf("Operation: ");
+        input = read_input();
+        if (input == NULL)
+            continue; // avoid empty input
+        op = strtok(input, " ");
+        // free(input);
+        if (strcmp(op, "get") == 0) {
+            param = strtok(NULL, " ");
+            get(sd, param);
+        }
+        else if (strcmp(op, "quit") == 0) {
+            quit(sd);
+            break;
+        }
+        else {
+            // new operations in the future
+            printf("TODO: unexpected command\n");
+        }
+        free(input);
+    }
+    free(input);
+}
+int validate_number(char *str) {
+   while (*str) {
+      if(!isdigit(*str)){ //if the character is not a number, return false
+         return 0;
+      }
+      str++; //point to next character
+   }
+   return 1;
+}
+int validate_ip(char *ip) { //check whether the IP is valid or not
+   int i, num, dots = 0;
+   char *ptr;
+   if (ip == NULL)
+      return 0;
+      ptr = strtok(ip, "."); //cut the string using dor delimiter
+      if (ptr == NULL)
+         return 0;
+   while (ptr) {
+      if (!validate_number(ptr)) //check whether the sub string is holding only number or not
+         return 0;
+         num = atoi(ptr); //convert substring to number
+         if (num >= 0 && num <= 255) {
+            ptr = strtok(NULL, "."); //cut the next part of the string
+            if (ptr != NULL)
+               dots++; //increase the dot count
+         } else
+            return 0;
+    }
+    if (dots != 3) //if the number of dots are not 3, return false
+       return 0;
+      return 1;
+}
+/**
+ * Run with
+ *         ./myftp <SERVER_IP> <SERVER_PORT>
+ **/
+int validate_port(char *port){
+  int num;
+	if (!validate_number(port)) //check whether the sub string is holding only number or not
+	    return 0;
+	    num = atoi(port); //convert substring to number
+	    if (num >= 1024 && num <= 65535) {
+	        return 1;
+	    } else
+	       return 0;
+}
+
+
+int main (int argc, char *argv[]) {
+    int sd, i;
+    struct sockaddr_in addr;
+
+    // arguments checking
+    for (i = 0; i < argc; i++){
+        printf("%s\n", argv[i]);
+    }
+    validate_ip(argv[1]) ? printf("IP Valida \n") : printf("No anda brou\n");
+    validate_port(argv[2]) ? printf("Puerto Valido \n") : printf("No anda brou\n");
+    // create socket and check for errors
+
+    // set socket data
+
+    // connect and check for errors
+
+    // if receive hello proceed with authenticate and operate if not warning
+
+    // close socket
+
+    return 0;
+}
